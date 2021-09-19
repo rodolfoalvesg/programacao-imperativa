b61262524a1971d61c4b258a9a39dbc0102a00f4
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2021-09-19 19:58:24 -0300

    aula15
    Introdução a arrays

---
+++ app.js
@@ -0,0 +1,7 @@
+let frutasAmarelas = ['Melão', 'Mamão', 'Limão'];
+let [fruta1, fruta2, fruta3] = frutasAmarelas;
+
+console.log(fruta1)
+console.log(fruta2)
+console.log(fruta3)
+console.log(frutasAmarelas)
